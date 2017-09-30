18d785796d99ad2b22cb9e163d62af3e66738e64
emcmurray <emcmurray@aevee.com>
2017-09-30 10:48:50 -0400

    cleaned up command line displayed text and if streamer not found didn't launch web page

--- BricastiStreamerSetup/Main.c
+++ BricastiStreamerSetup/Main.c
@@ -15,7 +15,8 @@ int parse_ping_for_IP(char * buffer, FILE * fp)
 
 	// setup ptrs to the IP address
 	fread(buff_1, 99, 1, fp);
-	printf("\n\n this is the buffer %s\n\n\n", buff_1);
+//	printf("\nLooking for Bricasti Streamer on Local Network\n");
+//	printf("\n\n this is the buffer %s\n\n\n", buff_1);
 
 	// check if no streamer is connected to network
 	start_ptr = strstr(buff_1, "could not find");
@@ -41,22 +42,35 @@ int main(void)
 {
 	char buffer[40] = { "start /max http://" };			// prebuild the launch web page cmd string
 	FILE *fp;
+	int return_data;
 
 	// create and open text file
-	system("an_5p > temp.txt");
+//	system("an_5p > temp.txt");
+	system("echo null> temp.txt");
+	printf("\nSearching for Bricasti Streamer on Local Network\n\n");
+
 	if ((fp = fopen("temp.txt", "r")) == NULL) return (-1);
 
 	// ping and check for any streamer, exit if not
 	system("/WINDOWS/System32/ping.exe -a AeVeeStreamer -4 > temp.txt");		// was AeVeeStreamer : ping host/domain for IPv4 IP address -a maybe reverse dns lookup
+//	system("/WINDOWS/System32/ping.exe -a office_2 -4 > temp.txt");
 	//    system("ping -a AeVeeStreamer > temp.txt");
  
 	// streamer present get IP address(s)
-	parse_ping_for_IP(&buffer[18], fp);
-	printf("this is IP buffer: %s\n\n", buffer);
-//	Sleep(10000);
+	return_data = parse_ping_for_IP(&buffer[18], fp);
+	if (return_data == 0)
+	{
+		printf("Bricasti Streamer not found on Local Network\n");
+		Sleep(4000);
+	}
+	else
+	{
+		printf("Launch Bricasti Streamer Setup webpage: %s\n\n", &buffer[11]);
+		Sleep(4000);
 
-	// execute full web page launch cmd
-	system(buffer);
+		// execute full web page launch cmd
+		system(buffer);
+	}
 
 	// close and delete text file
 	fclose(fp);
